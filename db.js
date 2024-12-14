b4d31932d234f8dd3eb8e550340c199a94fe2a37
Pranay Kotagiri <63542243+Pranay84@users.noreply.github.com>
2024-12-14 19:46:42 +0530

    db change

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const {Client} = require("pg")
 
 const client = new Client({
-    host: 'db.citiifuiprmpkxayffse.supabase.co',
+    host: 'aws-0-ap-south-1.pooler.supabase.com',
     database: 'postgres',
-    user: 'postgres',
+    user: 'postgres.citiifuiprmpkxayffse',
     password: 'Pranay@8118',
-    port: 5432
+    port: 6543
 })
 
 module.exports = client
